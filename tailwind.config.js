8813e296f82d07073fe954ccd8e88120e803bab0
Maxim <maxtrekusovsite@gmail.com>
2024-02-04 23:14:02 +0300

    add header

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // Plugin with dynamic selectors for '.icon-'
+    addDynamicIconSelectors(),
+    // Plugin with dynamic selectors that contains
+    // only css for overriding icon for '.icon-hover-'
+    addDynamicIconSelectors({
+      prefix: 'icon-hover',
+      overrideOnly: true,
+    }),
+  ],
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+const mode = process.env.NODE_ENV
+
+module.exports = {
+  mode,
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/img'),
+          to: path.resolve(__dirname, 'dist/img'),
+        },
+        {
+          from: path.resolve(__dirname, 'public/style.css'),
+          to: path.resolve(__dirname, 'dist/style.css'),
+        },
+      ],
+    }),
+
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        vendor: {
+          chunks: 'all',
+          name: 'vendor',
+          test: /node_modules/,
+        },
+      },
+    },
+  },
+}
